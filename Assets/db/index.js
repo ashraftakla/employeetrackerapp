904022bac7644c47b0586b76048d7196136f3a76
Ashraf Takla <ashraf.takla@icloud.com>
2020-11-24 10:34:42 -0500

    updated index.js file

--- Assets/db/index.js
+++ Assets/db/index.js
@@ -21,4 +21,11 @@ class DB {
     removeDepartment(departmentID) {
         return this.connection.query("DELETE FROM department WHERE id=?", departmentID)
     }
+    findAllEmployees() {
+        return this.connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+    }
+
 }
+module.exports = new DB(connection)
